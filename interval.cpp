8e91dcbc733cc6c88f71a87a144062fd4cfa5b69
Marios Zamparas <marioszamparas03@gmail.com>
2025-01-24 01:00:37 +0200

    added .h file. Needs proofreading (to check about correct syntax etc). TODO: endiamesos kwdikas

--- interval.cpp
+++ interval.cpp
@@ -1,264 +1,184 @@
-// Compilers semester 7th
+#include "interval.h"
+#include <algorithm>
+#include <initializer_list>
 
-#include <iostream>
-#include <stdexcept>
-#include <limits>
-#include <vector>
+// Interval class implementations
+Interval::Interval() : lower_bound(0), upper_bound(0), is_empty(true) {}
 
-class Interval
-{
-private:
-    double lower_bound; // 2 private variable για την εισαγωγη του κατω οριου και του ανω οριου
-    double upper_bound;
-    bool is_empty; // Μεταβλητη τυπου bool ειναι true ή false
-
-public:
-    // Constructor
-    Interval() : lower_bound(0), upper_bound(0), is_empty(true) {}
-
-    Interval(double lower, double upper) // Αρχικοποιηση του διαστηματος με τα δοσμενα κατω και ανω ορια
-    {
-        if (lower > upper) // Συγκρηση αν το κατω οριο ειναι μεγαλυτερο απο το ανω
-        {
-            std::cout << "Lower bound must be less than or equal to upper bound."; // Αν ισχυει βγαζει error
-            lower_bound = 0;                                                       // Εισαγωγη 0 ετσι ωστε σε περιπτωση εισαγωγης λαθος τιμης να υπαρχει ενα γνωστο default value χωρις να crasharei το program
-            upper_bound = 0;
-        }
-        else
-        {
-            lower_bound = lower; // Εισαγει τις τιμες του κατω και ανω οριου
-            upper_bound = upper;
-            is_empty = false;
-        }
+Interval::Interval(double lower, double upper) {
+    if (lower > upper) {
+        std::cout << "Lower bound must be less than or equal to upper bound.";
+        lower_bound = 0;
+        upper_bound = 0;
+    } else {
+        lower_bound = lower;
+        upper_bound = upper;
+        is_empty = false;
     }
+}
 
-    // Στατικος τροπος δημιουργιας empty interval
-    static Interval Empty()
-    {
+Interval Interval::Empty() {
+    return Interval();
+}
+
+Interval Interval::infinite() {
+    return Interval(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+}
+
+double Interval::lower() const {
+    if (is_empty) {
+        std::cout << "Interval is empty";
+    }
+    return lower_bound;
+}
+
+double Interval::upper() const {
+    if (is_empty) {
+        std::cout << "Interval is empty.";
+    }
+    return upper_bound;
+}
+
+bool Interval::isEmpty() const {
+    return is_empty;
+}
+
+Interval Interval::add(const Interval &other) const {
+    return Interval(lower_bound + other.lower_bound, upper_bound + other.upper_bound);
+}
+
+Interval Interval::operator+(const Interval &other) const {
+    return add(other);
+}
+
+Interval Interval::subtract(const Interval &other) const {
+    return Interval(lower_bound - other.upper_bound, upper_bound - other.lower_bound);
+}
+
+Interval Interval::multiply(const Interval &other) const {
+    double ll = lower_bound * other.lower_bound;
+    double lu = lower_bound * other.upper_bound;
+    double ul = upper_bound * other.lower_bound;
+    double uu = upper_bound * other.upper_bound;
+
+    double new_lower = *std::min_element(std::initializer_list<double>{ll, lu, ul, uu}.begin(),
+                                         std::initializer_list<double>{ll, lu, ul, uu}.end());
+    double new_upper = *std::max_element(std::initializer_list<double>{ll, lu, ul, uu}.begin(),
+                                         std::initializer_list<double>{ll, lu, ul, uu}.end());
+
+    return Interval(new_lower, new_upper);
+}
+
+Interval Interval::division(const Interval &other) const {
+    if (is_empty || other.is_empty) {
+        return Empty();
+    }
+    if (other.lower_bound <= 0 && other.upper_bound >= 0) {
+        std::cout << "\nDivision with 0 can't happen.";
         return Interval();
     }
 
-    // Στατικος τροπος δημιουργιας inf interval
-    static Interval infinite()
-    {
-        return Interval(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+    double ll = lower_bound / other.lower_bound;
+    double lu = lower_bound / other.upper_bound;
+    double ul = upper_bound / other.lower_bound;
+    double uu = upper_bound / other.upper_bound;
+
+    double new_lower = *std::min_element(std::initializer_list<double>{ll, lu, ul, uu}.begin(),
+                                         std::initializer_list<double>{ll, lu, ul, uu}.end());
+    double new_upper = *std::max_element(std::initializer_list<double>{ll, lu, ul, uu}.begin(),
+                                         std::initializer_list<double>{ll, lu, ul, uu}.end());
+
+    return Interval(new_lower, new_upper);
+}
+
+void Interval::print() const {
+    if (is_empty) {
+        std::cout << "Empty Interval";
+    } else {
+        std::cout << "[" << lower_bound << ", " << upper_bound << "]";
     }
+}
 
-    // Getter functions
-    double lower() const
-    {
-        if (is_empty)
-        {
-            std::cout << "Interval is empty";
-        }
-        return lower_bound;
+// IntervalVector class implementations
+IntervalVector::IntervalVector(size_t size) : intervals(size, Interval()) {}
+
+IntervalVector::IntervalVector(size_t size, double arr[][2]) {
+    for (size_t i = 0; i < size; ++i) {
+        intervals.emplace_back(arr[i][0], arr[i][1]);
     }
+}
 
-    double upper() const
-    {
-        if (is_empty)
-        {
-            std::cout << "Interval is empty.";
-        }
-        return upper_bound;
+Interval& IntervalVector::operator[](size_t index) {
+    if (index >= intervals.size()) {
+        throw std::out_of_range("Index out of range");
     }
+    return intervals[index];
+}
 
-    bool isEmpty() const
-    {
-        return is_empty;
+const Interval& IntervalVector::operator[](size_t index) const {
+    if (index >= intervals.size()) {
+        throw std::out_of_range("Index out of range");
     }
+    return intervals[index];
+}
 
-    // Add two intervals
-    Interval add(const Interval &other) const // Ληψει ενος αλλου αντικειμενου Interval ως αναφορα για να αποφευχθει η αντιγραφη
-    {
-        return Interval(lower_bound + other.lower_bound, upper_bound + other.upper_bound); // Δημιουργια ενος νεου Interval αντικειμενου με την τελικη προσθεση
+IntervalVector IntervalVector::add(const IntervalVector &other) const {
+    if (intervals.size() != other.intervals.size()) {
+        throw std::invalid_argument("Size mismatch for addition");
     }
-
-    // Overload operator+
-    Interval operator+(const Interval &other) const
-    {
-        return add(other);
+    IntervalVector result(intervals.size());
+    for (size_t i = 0; i < intervals.size(); ++i) {
+        result[i] = intervals[i].add(other.intervals[i]);
     }
+    return result;
+}
 
-    // Subtract two intervals
-    Interval subtract(const Interval &other) const // Ιδιο με το απο πανω
-    {
-        return Interval(lower_bound - other.upper_bound, upper_bound - other.lower_bound); // Δημιουργια ενος νεου Interval αντικειμενου με την τελικη αφαιρεση
+IntervalVector IntervalVector::multiply(const IntervalVector &other) const {
+    if (intervals.size() != other.intervals.size()) {
+        throw std::invalid_argument("Size mismatch for multiplication");
     }
-
-    // Multiply two intervals
-    Interval multiply(const Interval &other) const // Ιδιο με το απο πανω
-    {
-        double ll = lower_bound * other.lower_bound;
-        double lu = lower_bound * other.upper_bound;
-        double ul = upper_bound * other.lower_bound;
-        double uu = upper_bound * other.upper_bound; // Υπολογισμος ολων των πιθανων γινομενω των οριων
-
-        double new_lower = ll; // Υποθεση οτι το ll ειναι το μικροτερο γινομενο
-        if (lu < new_lower)
-            new_lower = lu;
-        if (ul < new_lower)
-            new_lower = ul;
-        if (uu < new_lower)
-            new_lower = uu;
-
-        double new_upper = ll;
-        if (lu > new_upper)
-            new_upper = lu;
-        if (ul > new_upper)
-            new_upper = ul;
-        if (uu > new_upper)
-            new_upper = uu;
-
-        return Interval(new_lower, new_upper);
+    IntervalVector result(intervals.size());
+    for (size_t i = 0; i < intervals.size(); ++i) {
+        result[i] = intervals[i].multiply(other.intervals[i]);
     }
+    return result;
+}
 
-    // Divide 2 intervals
-    Interval division(const Interval &other) const
-    {
-        if (is_empty || other.is_empty)
-        {
-            return Empty();
-        }
-        if (other.lower_bound <= 0 && other.upper_bound >= 0)
-        {
-            std::cout << "\nDivision with 0 cant happen.";
-            return Interval();
-        }
-
-        double ll = lower_bound / other.lower_bound;
-        double lu = lower_bound / other.upper_bound;
-        double ul = upper_bound / other.lower_bound;
-        double uu = upper_bound / other.upper_bound;
-
-        double new_lower = ll; // Ιδιος τροπος με το multiplication
-        if (lu < new_lower)
-            new_lower = lu;
-        if (ul < new_lower)
-            new_lower = ul;
-        if (uu < new_lower)
-            new_lower = uu;
-
-        double new_upper = ll;
-        if (lu > new_upper)
-            new_upper = lu;
-        if (ul > new_upper)
-            new_upper = ul;
-        if (uu > new_upper)
-            new_upper = uu;
-
-        return Interval(new_lower, new_upper);
+IntervalVector IntervalVector::subtract(const IntervalVector &other) const {
+    if (intervals.size() != other.intervals.size()) {
+        throw std::invalid_argument("Size mismatch for subtraction");
     }
-    // Εκτυπωση interval
-    void print() const
-    {
-        if (is_empty)
-        {
-            std::cout << "Empty Interval";
-        }
-        else
-        {
-            std::cout << "[" << lower_bound << ", " << upper_bound << "]";
-        }
+    IntervalVector result(intervals.size());
+    for (size_t i = 0; i < intervals.size(); ++i) {
+        result[i] = intervals[i].subtract(other.intervals[i]);
     }
-};
+    return result;
+}
 
-class IntervalVector {
-private:
-    std::vector<Interval> intervals;
-
-public:
-    IntervalVector(size_t size) : intervals(size, Interval()) {}
-    IntervalVector(size_t size, double arr[][2]) {
-        for (size_t i = 0; i < size; ++i) {
-            intervals.push_back(Interval(arr[i][0], arr[i][1]));
-        }
+IntervalVector IntervalVector::division(const IntervalVector &other) const {
+    if (intervals.size() != other.intervals.size()) {
+        throw std::invalid_argument("Size mismatch for division");
     }
-
-    Interval& operator[](size_t index) {
-        if (index >= intervals.size()) {
-            throw std::out_of_range("Index out of range");
-        }
-        return intervals[index];
+    IntervalVector result(intervals.size());
+    for (size_t i = 0; i < intervals.size(); ++i) {
+        result[i] = intervals[i].division(other.intervals[i]);
     }
+    return result;
+}
 
-    const Interval& operator[](size_t index) const {
-        if (index >= intervals.size()) {
-            throw std::out_of_range("Index out of range");
-        }
-        return intervals[index];
+size_t IntervalVector::size() const {
+    return intervals.size();
+}
+
+void IntervalVector::print() const {
+    for (const auto &interval : intervals) {
+        interval.print();
+        std::cout << " ";
     }
+    std::cout << std::endl;
+}
 
-    IntervalVector operator+(const IntervalVector& other) const {
-        if (intervals.size() != other.intervals.size()) {
-            throw std::invalid_argument("Size mismatch for addition");
-        }
-        IntervalVector result(intervals.size());
-        for (size_t i = 0; i < intervals.size(); ++i) {
-            result[i] = intervals[i] + other.intervals[i];
-        }
-        return result;
-    }
-
-    IntervalVector add(const IntervalVector& other) const {
-        if (intervals.size() != other.intervals.size()) {
-            throw std::invalid_argument("Size mismatch for addition");
-        }
-        IntervalVector result(intervals.size());
-        for (size_t i = 0; i < intervals.size(); ++i) {
-            result[i] = intervals[i].add(other.intervals[i]);
-        }
-        return result;
-    }
-    
-
-    IntervalVector multiply(const IntervalVector& other) const {
-        if (intervals.size() != other.intervals.size()) {
-            throw std::invalid_argument("Size mismatch for multiplication");
-        }
-        IntervalVector result(intervals.size());
-        for (size_t i = 0; i < intervals.size(); ++i) {
-            result[i] = intervals[i].multiply(other.intervals[i]);
-        }
-        return result;
-    }
-
-    IntervalVector subtract(const IntervalVector& other) const {
-        if (intervals.size() != other.intervals.size()) {
-            throw std::invalid_argument("Size mismatch for subtraction");
-        }
-        IntervalVector result(intervals.size());
-        for (size_t i = 0; i < intervals.size(); ++i) {
-            result[i] = intervals[i].subtract(other.intervals[i]);
-        }
-        return result;
-    }
-
-    IntervalVector division(const IntervalVector& other) const {
-        if (intervals.size() != other.intervals.size()) {
-            throw std::invalid_argument("Size mismatch for division");
-        }
-        IntervalVector result(intervals.size());
-        for (size_t i = 0; i < intervals.size(); ++i) {
-            result[i] = intervals[i].division(other.intervals[i]);
-        }
-        return result;
-    }
-
-    size_t size() const {
-        return intervals.size();
-    }
-
-    void print() const {
-        for (const auto& interval : intervals) {
-            interval.print();
-            std::cout << " ";
-        }
-        std::cout << std::endl;
-    }
-};
-
-
+// Main function
 int main()
 {
     // Δημιουργια 2 intervals
